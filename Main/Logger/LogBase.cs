6fac4b64a949bd4bbccf907c5f14392b50cf3c88
kuboshu83 <dot.sohkubo@gmail.com>
2024-06-08 19:16:29 +0900

    実行時にApplicationIdが何回も変化する不具合修正

--- Main/Logger/LogBase.cs
+++ Main/Logger/LogBase.cs
@@ -2,7 +2,7 @@
 
 public class LogBase(string applicationId)
 {
-	private static string _logId => $"log-{Guid.NewGuid().ToString("N")}";
+	private static readonly string _logId = $"log-{Guid.NewGuid().ToString("N")}";
 	public string LogId => _logId;
 	public DateTime Timestamp => DateTime.Now;
 	public string ApplicationId => applicationId;
